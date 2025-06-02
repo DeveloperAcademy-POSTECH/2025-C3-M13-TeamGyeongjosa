0775c9882610ae1dd46e528676a6232783efd729
G.M_YOO <ykm7003@gmail.com>
2025-06-02 17:45:21 +0900

    feat: 디자인 시스템 구축

---
+++ chukapoka/chukapoka/Core/DesignSystem/GSColor.swift
@@ -0,0 +1,19 @@
+//
+//  GSColor.swift
+//  chukapoka
+//
+//  Created by Demian Yoo on 6/2/25.
+//
+
+import SwiftUI
+
+enum GSColor {
+    static let primary: Color = Color("Primary1")
+    static let secondary1: Color = Color("Secondary1")
+    static let secondary2: Color = Color("Secondary2")
+    static let seccondary3: Color = Color("Secondary3")
+    static let accentColor: Color = Color("Accent1")
+    static let gray1: Color = Color("Gray1")
+    static let black: Color = Color("Black1")
+    static let white: Color = Color.white
+}
---
+++ chukapoka/chukapoka/Core/DesignSystem/GSFont.swift
@@ -0,0 +1,18 @@
+//
+//  GSFont.swift
+//  chukapoka
+//
+//  Created by Demian Yoo on 6/2/25.
+//
+import SwiftUI
+
+enum GSFont {
+    /// font: Pretendard, size: 24, weight: bold
+    static let title: Font = .custom("Pretendard-Bold", size: 24)
+    /// font: Pretendard, size: 16, weight: medium
+    static let subExplain: Font = .custom("Pretendard-Medium", size: 16)
+    /// font: Pretendard, size: 16, weight: bold
+    static let buttonText: Font = .custom("Pretendard-Bold", size: 16)
+    /// font: Pretendard, size: 16, weight: medium
+    static let placeholderText: Font = .custom("Pretendard-Medium", size: 16)
+}
