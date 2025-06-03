a54d2786f7c53825bb2ad6c1cfdd10f4fda55dd4
G.M_YOO <ykm7003@gmail.com>
2025-06-04 00:36:49 +0900

    refactor: 구조 리팩터링

--- chukapoka/chukapoka/Feature/Home/HomeView.swift
+++ chukapoka/chukapoka/Feature/Home/HomeView.swift
@@ -11,69 +11,26 @@ struct HomeView: View {
     @EnvironmentObject var coordinator: AppCoordinator
 
     var body: some View {
-        mainContent()
-    }
-    
-    // MARK: - Main View
-    @ViewBuilder
-    private func mainContent() -> some View {
         VStack(spacing: 0) {
             Spacer()
-            introSection()
+            HomeIntroText()
+                .padding(.bottom, 96)
+            HomeChracterImage()
             Spacer()
-            actionSection()
-                .padding(.bottom, 58)
-        }
-    }
-    
-    // MARK: - 상단 소개 문구 + 이미지
-    @ViewBuilder
-    private func introSection() -> some View {
-        VStack(spacing: 96) {
-            introText
-            pokiImage
-        }
-    }
-    
-    private var introText: some View {
-        Text("멤버를 초대하고\n함께 축하해보세요")
-            .multilineTextAlignment(.center)
-            .font(GSFont.title2)
-            .foregroundColor(GSColor.black)
-    }
-    
-    private var pokiImage: some View {
-        GSImage.mainLogo
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .frame(width: 214, height: 214)
-    }
-    
-    // MARK: - 하단 버튼 섹션
-    @ViewBuilder
-    private func actionSection() -> some View {
-        PairButton(
-            leftTitle: "그룹 생성하기",
-            rightTitle: "코드로 참여하기",
-            leftStyle: .custom(
-                textColor: GSColor.primary,
-                backgroundColor: GSColor.secondary3,
-                isEnable: true),
-            rightStyle: .custom(
-                textColor: GSColor.white,
-                backgroundColor: GSColor.primary,
-                isEnable: true),
-            leftAction: {
-                coordinator.push(.groupCreate(.infoStep1))
-            },
-            rightAction: {
+            HomeBottomButtons(
+                onGroupCreateTapped: {
+                    coordinator.push(.groupCreate(.infoStep1))
+                },
+                onGroupJoinTapped: {
                     coordinator.push(.groupJoin(.enterCode))
-            }
-        )
-        .padding(.horizontal, 16)
+                }
+            )
+            .padding(.bottom, 16)
+        }
     }
 }
 
 #Preview {
     HomeView()
+        .environmentObject(AppCoordinator())
 }
