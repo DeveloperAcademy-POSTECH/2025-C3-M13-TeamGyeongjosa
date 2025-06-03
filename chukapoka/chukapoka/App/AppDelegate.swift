dc395ec1f952f5de95da8d931ce49158a95b31d1
G.M_YOO <ykm7003@gmail.com>
2025-06-03 14:05:24 +0900

    chore:#18
    - AppDelegate 테스트 코드 수정

--- chukapoka/chukapoka/App/AppDelegate.swift
+++ chukapoka/chukapoka/App/AppDelegate.swift
@@ -5,12 +5,12 @@
 //  Created by Demian Yoo on 5/31/25.
 //
 
-//import UIKit
-//import Foundation
+// import UIKit
+// import Foundation
 //
-//class AppDelegate: NSObject, UIApplicationDelegate {
+// class AppDelegate: NSObject, UIApplicationDelegate {
 //    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
 //        // 찐 앱 시작점
 //        return true
 //    }
-//}
+// }
