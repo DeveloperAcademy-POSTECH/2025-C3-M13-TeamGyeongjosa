599d5379b560e8c608270be4c72bda7d882531c5
wish627 <wishful.to@gmail.com>
2025-06-04 23:30:48 +0900

    feat: 메인 페이지에서 참여코드 입력 페이지 구현

--- chukapoka/chukapoka/Feature/NameCheck.swift
+++ chukapoka/chukapoka/Feature/NameCheck.swift
@@ -14,7 +14,7 @@ struct NameCheck: View {
     
     var body: some View {
         
-        VStack(alignment: .leading, spacing: 0){
+        VStack(alignment: .leading, spacing: 0) {
             
             (
                 Text("모임명\n") +
