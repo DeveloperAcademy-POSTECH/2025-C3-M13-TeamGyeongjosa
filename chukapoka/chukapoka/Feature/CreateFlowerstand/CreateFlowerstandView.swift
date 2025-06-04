9adf7a1a8a189372728a533287986a984655702e
ohcuy <everywavyday@gmail.com>
2025-06-04 22:32:48 +0900

    feat: CreateFlowerstand 기본 구조 세팅

---
+++ chukapoka/chukapoka/Feature/CreateFlowerstand/CreateFlowerstandView.swift
@@ -0,0 +1,22 @@
+//
+//  CreateFlowerstandView.swift
+//  chukapoka
+//
+//  Created by 조유진 on 6/4/25.
+//
+
+import SwiftUI
+
+struct CreateFlowerstandView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            CircleProgress()
+                .padding(.top, 48)
+            
+        }
+    }
+}
+
+#Preview {
+    CreateFlowerstandView()
+}
