e2d598604a2390c6c3ba943211ae1610fbe3c654
ohcuy <everywavyday@gmail.com>
2025-06-05 12:24:26 +0900

    feat: 데이터 타입 수정

--- chukapoka/chukapoka/Core/SharedModel/Wedding.swift
+++ chukapoka/chukapoka/Core/SharedModel/Wedding.swift
@@ -10,29 +10,28 @@ import SwiftData
 
 @Model
 final class Wedding {
-        @Attribute(.unique) var weddingID: Int
-        var place: String
-        var date: Date
-        var time: Date
-        var accountName: String
-        var accountNumber: Int
-
-        // 관계: Wedding ⟶ Party (1:N)
-        @Relationship(deleteRule: .cascade) var parties: [Party] = []
-
-        init(
-            weddingID: Int,
-            place: String,
-            date: Date,
-            time: Date,
-            accountName: String,
-            accountNumber: Int
-        ) {
-            self.weddingID = weddingID
-            self.place = place
-            self.date = date
-            self.time = time
-            self.accountName = accountName
-            self.accountNumber = accountNumber
-        }
+    @Attribute(.unique)
+    var weddingID: UUID
+    var place: String
+    var date: Date
+    var accountName: String
+    var accountNumber: String
+    
+    // 관계: Wedding ⟶ Party (1:N)
+    @Relationship(deleteRule: .cascade)
+    var parties: [Party] = []
+    
+    init(
+        weddingID: UUID = UUID(),
+        place: String,
+        date: Date,
+        accountName: String,
+        accountNumber: String
+    ) {
+        self.weddingID = weddingID
+        self.place = place
+        self.date = date
+        self.accountName = accountName
+        self.accountNumber = accountNumber
+    }
 }
