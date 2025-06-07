4b533e0c2f0da8ef055a4217f17ae255526335f1
ohcuy <everywavyday@gmail.com>
2025-06-07 19:57:39 +0900

    feat: ReceiveCongratsView 기능 구현

---
+++ chukapoka/chukapoka/Feature/ReceiveCongrats/SubViews/CardMemberListMemberSectionScrollView.swift
@@ -0,0 +1,25 @@
+//
+//  CardMemberListMemberSectionScrollView.swift
+//  chukapoka
+//
+//  Created by 조유진 on 6/7/25.
+//
+
+import SwiftUI
+
+struct CardMemberListMemberSectionScrollView: View {
+    let members: [PartyMember]
+    
+    var body: some View {
+        ScrollView {
+            ForEach(members, id: \.self) { member in
+                CardMemberListItem(
+                    name: member.name,
+                    money: member.money
+                )
+            }
+        }
+        .frame(maxHeight: 232)
+        .padding(.bottom, 26)
+    }
+}
