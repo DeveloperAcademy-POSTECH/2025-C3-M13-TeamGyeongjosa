02820fbd4a5679c48808de476b871596093333c1
Jenki <37648677+ADA-Jenki@users.noreply.github.com>
2025-06-11 03:13:20 +0900

    feat: PhotoPickerView에 청첩장 이미지 삽입

--- chukapoka/chukapoka/Core/DesignSystem/GSImage.swift
+++ chukapoka/chukapoka/Core/DesignSystem/GSImage.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 enum GSImage {
     static let mainLogo: Image = Image("MainPoki")
+    static let invitaionCard: Image = Image("InvitationCard")
     static let redFlower: Image = Image("RedFlower")
     static let blueFlower: Image = Image("BlueFlower")
     static let yellowFlower: Image = Image("YellowFlower")
--- chukapoka/chukapoka/Feature/CreateGroupOCR/View/SubViews/Step1/PhotoPickerView.swift
+++ chukapoka/chukapoka/Feature/CreateGroupOCR/View/SubViews/Step1/PhotoPickerView.swift
@@ -19,13 +19,12 @@ struct PhotoPickerView: View {
     }
     
     var body: some View {
+        // MARK: - 툴바 (뒤로가기)
+        NavigationBar {
+            viewModel.goToPreviousStep()
+        }
+        CustomProgressView(progress: 0.3).padding(.bottom, 30)
         VStack(alignment: .leading, spacing: 4) {
-            // MARK: - 툴바 (뒤로가기)
-            NavigationBar {
-                viewModel.goToPreviousStep()
-            }
-            //            CustomProgressView(progress: viewModel.progressRate)
-            .padding(.bottom, 30)
             // MARK: - 청첩장 불러오기 설명
             Text("화환을 함께 전달하기 위해\n몇 가지 정보를 수집할게요")
                 .font(GSFont.title2)
@@ -34,6 +33,13 @@ struct PhotoPickerView: View {
             Text("청첩장 이미지를 불러오면 시간이 단축돼요")
                 .font(GSFont.caption2)
                 .foregroundStyle(GSColor.gray2)
+            // MARK: - 이미지 삽입
+            Spacer()
+            HStack {
+                GSImage.invitaionCard
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
             Spacer()
             // MARK: - 청첩장 불러오기 버튼
             PrimaryButton(
