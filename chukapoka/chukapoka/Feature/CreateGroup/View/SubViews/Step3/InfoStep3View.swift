e405f17dc7d6d9e297a295144dc75f44efc1b7fd
ohcuy <everywavyday@gmail.com>
2025-06-11 16:15:01 +0900

    refactor: CustomTextField 수정으로 인한 CreateGroupViewModel 수정 및 View에 반영

--- chukapoka/chukapoka/Feature/CreateGroup/View/SubViews/Step3/InfoStep3View.swift
+++ chukapoka/chukapoka/Feature/CreateGroup/View/SubViews/Step3/InfoStep3View.swift
@@ -13,28 +13,31 @@ struct InfoStep3View: View {
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 32) {
-                Text("화환을 보내는 분의 정보를 수집할게요")
+                Text("화환을 보내는 분의\n정보를 수집할게요")
                     .font(GSFont.title2)
                 
                 CustomTextField(
                     title: "보내는 분",
                     placeholder: "ex) 김포키",
                     text: $viewModel.senderName,
-                    isValid: $viewModel.isSenderNameValid
+                    isValid: $viewModel.isSenderNameValid,
+                    errorMessage: "보내는 분은 한글 10자 이내로 입력해주세요"
                 )
                 
                 CustomTextField(
-                    title: "은행",
-                    placeholder: "ex) 카카오뱅크",
+                    title: "은행명",
+                    placeholder: "화환을 보내는 분의 은행을 입력해주세요",
                     text: $viewModel.senderBank,
-                    isValid: $viewModel.isSenderBankValid
+                    isValid: $viewModel.isSenderBankValid,
+                    errorMessage: "은행명은 한글 10자 이내로 입력해주세요"
                 )
                 
                 CustomTextField(
                     title: "계좌번호",
-                    placeholder: "계좌번호 입력",
+                    placeholder: "화환을 보내는 분의 계좌번호를 입력해주세요",
                     text: $viewModel.senderAccount,
-                    isValid: $viewModel.isSenderAccountValid
+                    isValid: $viewModel.isSenderAccountValid,
+                    errorMessage: "계좌번호는 11자 ~ 14자 이내로 입력해주세요"
                 )
                 .keyboardType(.numberPad)
                 
@@ -42,14 +45,15 @@ struct InfoStep3View: View {
                     title: "연락처",
                     placeholder: "010-1234-5678",
                     text: $viewModel.senderPhoneNumber,
-                    isValid: $viewModel.isSenderPhoneNumberValid
+                    isValid: $viewModel.isSenderPhoneNumberValid,
+                    errorMessage: "연락처는 11자 이내로 입력해주세요"
                 )
                 .keyboardType(.numberPad)
                 .onChange(of: viewModel.senderPhoneNumber) {
                     viewModel.senderPhoneNumber = viewModel.formatPhoneNumberInput(viewModel.senderPhoneNumber)
                 }
                 
-                Text("파티 해산 시 해당 정보로 돌려드려요")
+                Text("파티가 해산되면 입력하신 정보로 돌려드려요")
                     .font(GSFont.caption2)
                     .foregroundColor(GSColor.black)
                     .frame(maxWidth: .infinity)
